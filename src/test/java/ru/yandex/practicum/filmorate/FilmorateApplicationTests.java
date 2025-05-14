af880f2bd4b37ef1360c2406b5746568096638f8
vil-shavetail <demid.ivan@gmail.com>
2025-05-14 20:37:38 +0300

    Sprint 10. Added tests for models and simple test for checking controllers.

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,25 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
 
 @SpringBootTest
 class FilmorateApplicationTests {
 
+	@Autowired
+	UserController userController;
+
+	@Autowired
+	FilmController filmController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(userController.findAll().isEmpty());
+		Assertions.assertTrue(filmController.findAll().isEmpty());
 	}
 
 }
