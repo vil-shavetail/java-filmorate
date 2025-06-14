175d71f72e8f24a4a0e3806894fdbddc2ebd4bbc
vil-shavetail <demid.ivan@gmail.com>
2025-06-14 17:18:53 +0300

    Sprint 12. Initial commit. Added database diagram and script for creating database.

---
+++ src/main/resources/mpa_rate.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.mpa_rate (mpa_rate_name, mpa_rate_description) VALUES ('G', 'у фильма нет возрастных ограничений');
+INSERT INTO public.mpa_rate (mpa_rate_name, mpa_rate_description) VALUES ('PG', 'детям рекомендуется смотреть фильм с родителями');
+INSERT INTO public.mpa_rate (mpa_rate_name, mpa_rate_description) VALUES ('PG-13', 'детям до 13 лет просмотр не желателен');
+INSERT INTO public.mpa_rate (mpa_rate_name, mpa_rate_description) VALUES ('R', 'лицам до 17 лет просматривать фильм можно только в присутствии взрослого');
+INSERT INTO public.mpa_rate (mpa_rate_name, mpa_rate_description) VALUES ('NC-17', 'лицам до 18 лет просмотр запрещён');
