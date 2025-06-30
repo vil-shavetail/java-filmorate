d2d3e73e973ee46d16fc4d051174e0d3b51e974c
vil-shavetail <demid.ivan@gmail.com>
2025-06-30 22:49:57 +0300

    Sprint 12. Fixed Postman errors.

--- src/main/java/ru/yandex/practicum/filmorate/model/MpaRate.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRate.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import lombok.Data;
 @Builder
 public class MpaRate {
     private int id;
+    @NotNull
     private String name;
 //    private String description;
 }
