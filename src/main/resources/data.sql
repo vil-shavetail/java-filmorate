d899f491ef6e51e9848c193068a9cfc7db6f4566
vil-shavetail <demid.ivan@gmail.com>
2025-07-01 09:54:28 +0300

    Sprint 12. Fixed Postman errors.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,31 +2,31 @@ insert into mpa_rate (mpa_rate_name, mpa_rate_description)
 select 'G',
        'у фильма нет возрастных ограничений'
     where not exists (select 1 from mpa_rate
-        where mpa_rate_description = 'G'
+        where mpa_rate_name = 'G'
             and mpa_rate_description = 'у фильма нет возрастных ограничений');
 insert into mpa_rate (mpa_rate_name, mpa_rate_description)
 select 'PG',
        'детям рекомендуется смотреть фильм с родителями'
     where not exists (select 1 from mpa_rate
-        where mpa_rate_description = 'PG'
+        where mpa_rate_name = 'PG'
             and mpa_rate_description = 'детям рекомендуется смотреть фильм с родителями');
 insert into mpa_rate (mpa_rate_name, mpa_rate_description)
 select 'PG-13',
        'детям до 13 лет просмотр не желателен'
 where not exists (select 1 from mpa_rate
-                  where mpa_rate_description = 'PG-13'
+                  where mpa_rate_name = 'PG-13'
                     and mpa_rate_description = 'детям до 13 лет просмотр не желателен');
 insert into mpa_rate (mpa_rate_name, mpa_rate_description)
 select 'R',
        'лицам до 17 лет просматривать фильм можно только в присутствии взрослого'
 where not exists (select 1 from mpa_rate
-                  where mpa_rate_description = 'R'
+                  where mpa_rate_name = 'R'
                     and mpa_rate_description = 'лицам до 17 лет просматривать фильм можно только в присутствии взрослого');
 insert into mpa_rate (mpa_rate_name, mpa_rate_description)
 select 'NC-17',
        'лицам до 18 лет просмотр запрещён'
 where not exists (select 1 from mpa_rate
-                  where mpa_rate_description = 'NC-17'
+                  where mpa_rate_name = 'NC-17'
                     and mpa_rate_description = 'лицам до 18 лет просмотр запрещён');
 
 insert into genre(genre_name)
